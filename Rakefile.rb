eed16020a3b92606ed6ed74d123425f6b0d7d2d2
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-07-13 04:02:35 +0000

    Adding bin to Rakefile
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@30 8c6f25ac-f8d4-496d-a293-71e089142da2

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,18 +3,21 @@ Gem::manage_gems
 require 'rake/gempackagetask'
 
 spec = Gem::Specification.new do |s|
-    s.name       = "dated_backup"
-    s.version    = "0.1.0"
-    s.author     = "Scott Taylor"
-    s.email      = "scott@railsnewbie.com"
-    s.homepage   = "http://rubyforge.org/projects/datedbackup"
-    s.platform   = Gem::Platform::RUBY
-    s.summary    = "Incremental Backups Using Rsync"
+    s.name      = "dated_backup"
+    s.version   = "0.1.1"
+    s.author    = "Scott Taylor"
+    s.email     = "scott@railsnewbie.com"
+    s.homepage  = "http://rubyforge.org/projects/datedbackup"
+    s.platform  = Gem::Platform::RUBY
+    s.summary   = "Incremental Dated Backups Using Rsync"
+                
+    s.files     = FileList["{lib,example_scripts,bin}/**/*"].to_a
     
-    s.files      = FileList["{lib,example_scripts}/**/*"].to_a
+    s.bindir   = 'bin'
+    s.executables = ["dbackup"]                
         
-    s.has_rdoc          = false
-    #s.extra_rdoc_files  = %w(README TODO DONE)
+    s.has_rdoc          = true
+    s.extra_rdoc_files  = %w(README DONE COPYRIGHT)
 end
 
 Rake::GemPackageTask.new(spec) do |pkg|
