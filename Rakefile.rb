91604237021b975c915bada06efacd4762df2afd
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-07-13 04:24:20 +0000

    Rakefile changes
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@31 8c6f25ac-f8d4-496d-a293-71e089142da2

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,23 +3,32 @@ Gem::manage_gems
 require 'rake/gempackagetask'
 
 spec = Gem::Specification.new do |s|
-    s.name      = "dated_backup"
-    s.version   = "0.1.1"
-    s.author    = "Scott Taylor"
-    s.email     = "scott@railsnewbie.com"
-    s.homepage  = "http://rubyforge.org/projects/datedbackup"
-    s.platform  = Gem::Platform::RUBY
-    s.summary   = "Incremental Dated Backups Using Rsync"
-                
-    s.files     = FileList["{lib,example_scripts,bin}/**/*"].to_a
-    
-    s.bindir   = 'bin'
-    s.executables = ["dbackup"]                
-        
-    s.has_rdoc          = true
-    s.extra_rdoc_files  = %w(README DONE COPYRIGHT)
+  s.name      = "dated_backup"
+  s.version   = "0.1.1"
+  s.author    = "Scott Taylor"
+  s.email     = "scott@railsnewbie.com"
+  s.homepage  = "http://rubyforge.org/projects/datedbackup"
+  s.platform  = Gem::Platform::RUBY
+  s.summary   = "Incremental Dated Backups Using Rsync"
+              
+  s.files     = FileList["{lib,example_scripts,bin}/**/*"].to_a + %w(README COPYRIGHT)
+  
+  s.bindir   = 'bin'
+  s.executables = ["dbackup"]                
+      
+  s.has_rdoc          = true
+  s.extra_rdoc_files  = %w(README DONE COPYRIGHT)
 end
 
 Rake::GemPackageTask.new(spec) do |pkg|
-    pkg.need_tar = true
+  pkg.need_tar = true
 end
+
+desc "Generate Documentation"
+task :rdoc do
+  %x(rm -rf doc/)
+  %x(rdoc)
+end
+
+desc "Build Release"
+task :build_release => [:rdoc, :repackage]
