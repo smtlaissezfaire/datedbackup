6a464638d37dcd0e82565e941098250c5b3dc416
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-07-17 00:56:54 +0000

    specs were not changed for the last commit
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@47 8c6f25ac-f8d4-496d-a293-71e089142da2

--- spec/dated_backup/dated_backup.rb
+++ spec/dated_backup/dated_backup.rb
@@ -2,25 +2,25 @@ require File.dirname(__FILE__) + "/../spec_helper"
 
 describe DatedBackup, "accessor methods" do
   it "should have source directory as array if only one provided" do
-    DatedBackup.new(:source => "dir").sources.should == ["dir"]
+    DatedBackup.new("source" => "dir").sources.should == ["dir"]
   end
   
   it "should have source directories as array if many provided" do
-    DatedBackup.new(:sources => ["dir1", "dir2"]).sources.should == ["dir1", "dir2"]
+    DatedBackup.new("sources" => ["dir1", "dir2"]).sources.should == ["dir1", "dir2"]
   end
   
   it "should assign the original destination as the backup_root" do
-    DatedBackup.new(:destination => "dest").backup_root.should == "dest"
+    DatedBackup.new("destination" => "dest").backup_root.should == "dest"
   end
   
   it "should have the options as a string" do
-    DatedBackup.new(:options => "opt1, opt2").options.should == "opt1, opt2"
+    DatedBackup.new("options" => "opt1, opt2").options.should == "opt1, opt2"
   end
   
   it "should have the destination as the destination dir/timestamp" do
     time = Time.now
     time_format = time.strftime "%Y-%m-%d-%Hh-%Mm-%Ss"
     
-    DatedBackup.new(:destination => "dest").destination.should == "dest/#{time_format}"
+    DatedBackup.new("destination" => "dest").destination.should == "dest/#{time_format}"
   end
 end
