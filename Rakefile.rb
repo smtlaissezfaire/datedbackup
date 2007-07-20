14380a6ea6de30a6f229b7f0208f1b4ea8de7b22
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-07-20 02:39:13 +0000

    adding updated Rakefile, which has rcov and rdoc going into documentation directory, plus 100% code verification
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@63 8c6f25ac-f8d4-496d-a293-71e089142da2

--- Rakefile.rb
+++ Rakefile.rb
@@ -25,24 +25,41 @@ Rake::GemPackageTask.new(spec) do |pkg|
   pkg.need_tar = true
 end
 
-desc "Generate Documentation"
-task :rdoc do
-  %x(rm -rf doc/)
-  %x(rdoc)
+desc "Generate Rdoc"
+task :rdoc => [:clobber_rdoc] do
+  %x(rdoc -o doc/rdoc)
 end
 
-desc "Build Release"
-task :build_release => [:rdoc, :repackage]
+desc "Remove Rdoc"
+task :clobber_rdoc do
+  %x(rm -rf doc/rdoc)
+end
 
+desc "Remove Rcov"
+task :clobber_rcov do
+  %x(rm -rf doc/rcov)
+end
+
+desc "Generate all documentation"
+task :generate_documentation => [:rdoc, :rcov]
+
+desc "Remove all documentation"
+task :clobber_documentation => [:clobber_rdoc, :clobber_rcov]
+
+desc "Build Release"
+task :build_release => [:pre_commit, :repackage]
 
 desc "Run all examples with RCov"
 Spec::Rake::SpecTask.new('rcov') do |t|
   t.spec_files = FileList['spec/**/*.rb']
   t.rcov = true
   t.rcov_opts = ['--exclude', 'spec']
+  t.rcov_dir = "doc/rcov"
 end
 
 RCov::VerifyTask.new(:verify_rcov => :spec) do |t|
   t.threshold = 100.0
-  t.index_html = 'coverage/index.html'
+  t.index_html = 'doc/rcov/index.html'
 end
+
+task :pre_commit => [:clobber_documentation, :generate_documentation, :verify_rcov]
