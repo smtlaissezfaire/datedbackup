55e354cbbd38f6350db6970a6a5644634ad98ea8
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-07-20 01:00:08 +0000

    refactored error checking out of db's initialization
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@58 8c6f25ac-f8d4-496d-a293-71e089142da2

--- spec/dated_backup/dated_backup.rb
+++ spec/dated_backup/dated_backup.rb
@@ -1,35 +1,49 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe DatedBackup, "accessor methods" do
+  before :each do
+    @kernel = mock 'Kernel'
+    @db = DatedBackup.new(@kernel)
+  end
+  
+  @db
+  
   it "should have source directory as array if only one provided" do
-    DatedBackup.new("source" => "dir").sources.should == ["dir"]
+    @db.set_attributes("source" => "dir")
+    @db.sources.should == ["dir"]
   end
   
   it "should have source directories as array if many provided" do
-    DatedBackup.new("sources" => ["dir1", "dir2"]).sources.should == ["dir1", "dir2"]
+    @db.set_attributes("sources" => ["dir1", "dir2"])
+    @db.sources.should == ["dir1", "dir2"]
   end
   
   it "should assign the original destination as the backup_root" do
-    DatedBackup.new("destination" => "dest").backup_root.should == "dest"
+    @db.set_attributes("destination" => "dest")
+    @db.backup_root.should == "dest"
   end
   
   it "should have the options as a string" do
-    DatedBackup.new("options" => "opt1, opt2").options.should == "opt1, opt2"
+    @db.set_attributes("options" => "opt1, opt2")
+    @db.options.should == "opt1, opt2"
   end
   
   it "should have the destination as the destination dir/timestamp" do
     time = Time.now
     time_format = time.strftime "%Y-%m-%d-%Hh-%Mm-%Ss"
     
-    DatedBackup.new("destination" => "dest").destination.should == "dest/#{time_format}"
+    @db.set_attributes("destination" => "dest")
+    @db.destination.should == "dest/#{time_format}"
   end
   
   it "should have the pre_run_commands as an array, if given many elements" do
-    DatedBackup.new("pre_run_commands" => ["cmd1", "cmd2"]).pre_run_commands.should == ["cmd1", "cmd2"]
+    @db.set_attributes("pre_run_commands" => ["cmd1", "cmd2"])
+    @db.pre_run_commands.should == ["cmd1", "cmd2"]
   end
   
   it "should have the pre_run_commands as an array if given one element" do
-    DatedBackup.new("pre_run_command" => "cmd1").pre_run_commands.should == ["cmd1"]
+    @db.set_attributes("pre_run_command" => "cmd1")
+    @db.pre_run_commands.should == ["cmd1"]
   end
 end
 
@@ -50,26 +64,39 @@ describe DatedBackup, "errors" do
       "sources" => ["something"],
       "destination" => ["something"]
     }
+    @kernel = mock 'Kernel'
+    @db = DatedBackup.new(@kernel)
   end
   
   it "should raise an error if not given a source directory" do
     h = @valid_hash.reject { |key, _| key == "sources" }
-    lambda { DatedBackup.new(h).check_for_directory_errors }.should raise_error(DirectoryError, "No source directory given")
+    @db.set_attributes(h)
+
+    lambda { 
+      @db.check_for_directory_errors 
+    }.should raise_error(DirectoryError, "No source directory given")
   end
   
   it "should raise an error if not given a destination directory" do
     h = @valid_hash.reject { |key, _| key == "destination"}
-    lambda { DatedBackup.new(h).check_for_directory_errors }.should raise_error(DirectoryError, "No destination directory given")
+    @db.set_attributes(h)
+    
+    lambda { 
+      @db.check_for_directory_errors 
+    }.should raise_error(DirectoryError, "No destination directory given")
   end
 end
 
 describe DatedBackup, "with invalid directories" do
   before :each do
     @kernel = mock 'Kernel'
+    @db = DatedBackup.new(@kernel)
   end
   
   it "should raise an error if the object was not given a directory" do
-    backup = DatedBackup.new({"pre_run_command" => "cmd1"}, @kernel)
-    lambda {backup.run}.should raise_error(DirectoryError)
+    @db.set_attributes({"pre_run_command" => "cmd1"})
+    lambda {
+      @db.run
+    }.should raise_error(DirectoryError)
   end
 end
