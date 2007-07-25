ed878e43ec9cfdea829a172edb3c7cd641b313fa
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-07-25 19:31:49 +0000

    this time actually just moving specs into core - last commit moved misc. files in lib into core
    git-svn-id: http://datedbackup.rubyforge.org/svn/branches/dsl@96 8c6f25ac-f8d4-496d-a293-71e089142da2

--- spec/dated_backup/core/command_line_spec.rb
+++ spec/dated_backup/core/command_line_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../spec_helper"
+require File.dirname(__FILE__) + "/../../spec_helper"
 
 command_line = DatedBackup::CommandLine
 include command_line
--- spec/dated_backup/core/tasks_spec.rb
+++ spec/dated_backup/core/tasks_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../spec_helper"
+require File.dirname(__FILE__) + "/../../spec_helper"
 
 module CommonMock
   def include_common_mocks
