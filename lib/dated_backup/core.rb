60a312787dbc73dba92441654a609b8ae4b4166d
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-07-30 07:43:04 +0000

    moving backup set into core dir
    git-svn-id: http://datedbackup.rubyforge.org/svn/branches/dsl@109 8c6f25ac-f8d4-496d-a293-71e089142da2

--- lib/dated_backup/core.rb
+++ lib/dated_backup/core.rb
@@ -1,4 +1,4 @@
-
+require File.dirname(__FILE__) + "/core/backup_set"
 require File.dirname(__FILE__) + "/core/tasks"
 require File.dirname(__FILE__) + "/core/command_line"
 require File.dirname(__FILE__) + "/core/backup_remover"
