6893a28d9dde4ba8d0cea7e26609d7083e858aca
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-07-31 21:15:17 +0000

    moving a string spec into the extension spec directory
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@138 8c6f25ac-f8d4-496d-a293-71e089142da2

---
+++ spec/dated_backup/extensions/string_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+
+describe String, "to_time" do
+  it "should create a valid time object from the string given" do
+    "dir/2006-01-25-12h-00m-32s".to_time.should == Time.gm('2006', 01, 25, 12, 00, 32)
+  end
+end
