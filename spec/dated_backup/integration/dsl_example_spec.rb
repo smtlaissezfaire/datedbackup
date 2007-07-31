7b41daf8021ada954711637d0c0eb87b97accac3
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-07-31 02:58:00 +0000

    more integration tests + fixed scripts
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@125 8c6f25ac-f8d4-496d-a293-71e089142da2

--- spec/dated_backup/integration/dsl_example_spec.rb
+++ spec/dated_backup/integration/dsl_example_spec.rb
@@ -24,3 +24,67 @@ describe "DSL Integration/Regression Test with example.com Script" do
     DatedBackup::ExecutionContext.new :main, @files 
   end
 end
+
+describe "DSL Integration/Regression Test with the local_etc_backup script" do
+  before :each do
+    @dbackup = mock DatedBackup::Core
+    DatedBackup::Core.stub!(:new).and_return @dbackup
+    @dbackup.stub!(:run).and_return @dbackup
+    @dbackup.stub!(:set_attributes).and_return @dbackup
+    @file = "example_scripts/local_etc_backup"
+  end
+
+  #source       '/etc'
+  #destination  '/root/etc_backup'
+  #
+  #after {
+  #  remove_old {
+  #    keep this weeks backups
+  #    keep monthly backups    
+  #  }
+  #}
+  
+  it "should parse the key + value hash correctly" do
+    @dbackup.should_receive(:set_attributes).with({
+      :source => ["/etc"],
+      :destination => ["/root/etc_backup"]
+    })
+    DatedBackup::ExecutionContext.new :main, @file
+  end
+end
+
+describe "DSL Integration/Regression Tests with the samba_shares script" do
+  before :each do
+    @dbackup = mock DatedBackup::Core
+    DatedBackup::Core.stub!(:new).and_return @dbackup
+    @dbackup.stub!(:run).and_return @dbackup
+    @dbackup.stub!(:set_attributes).and_return @dbackup
+    @file = "example_scripts/samba_shares"
+  end
+
+  # source      = '/mnt/shares'
+  # destination = '/var/backups/network/backups/shares'
+  # options     = '-v' 
+  # 
+  # after do
+  # 
+  #   # CAREFUL! This will remove all old backups
+  #   # except the ones specified inside the block by the 'keep' rules
+  #   remove_old do
+  #     keep this weeks backups 
+  #     keep last weeks backups 
+  #     keep weekly backups from this month # or: keep this months weekly backups
+  #     keep monthly backups # or: keep all monthly backups
+  #   end
+  # 
+  # end
+  
+  it "should parse the key + value hash correctly" do
+    @dbackup.should_receive(:set_attributes).with({
+      :source => ["/mnt/shares"],
+      :destination => ["/var/backups/network/backups/shares"],
+      :options => ["-v"]
+    })
+    DatedBackup::ExecutionContext.new :main, @file
+  end
+end
