f96dc53c1c0891e9e80db1869d7b3b128ee210a2
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-08-06 14:23:31 +0000

    finally testing the bi/dbackup file
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@173 8c6f25ac-f8d4-496d-a293-71e089142da2

--- spec/dated_backup/bin/dbackup_spec.rb
+++ spec/dated_backup/bin/dbackup_spec.rb
@@ -2,24 +2,23 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 
 describe DatedBackup, "loading from the binary" do
   before :each do
-    @dsl = mock DatedBackup::DSL::Main
-    @filename = mock String
-    @filename.stub!(:to_s).and_return @filename
+    @evaluation_object = Object.new
+    @evaluation_object.stub!(:require).and_return nil
+    DatedBackup::ExecutionContext.stub!(:new).and_return nil
+    @filename = "some_config"
+    
+    @old_argv = ARGV
+    @new_argv = [@filename]
+    ARGV = @new_argv
   end
   
-  # can't get this spec to work...
-  # tried instance_evaling the load, although I suppose
-  # that is futile, as the spec itself is already instance_eval'ed
-  # by RSpec's code, hence the ARGV variable propagates into the
-  # loaded binaries file environement (or rather the bin's file)
-  # gets imported into *this* environement.  The real mystery
-  # is why the stub on DSL.load doesn't carry over. 
-  it "should call the DSL with the first file argument" #do
-  #  DatedBackup::DSL::Main.stub!(:load).and_return nil
-  #  DatedBackup::DSL::Main.load.should == nil
-  #
-  #  DatedBackup::DSL::Main.should_receive(:load)
-  #  ARGV[0] = @filename
-  #  load File.dirname(__FILE__) + "/../../../bin/dbackup"      
-  #end
+  after :each do
+    ARGV = @old_argv
+  end
+  
+  it "should call the ExecutionContext with the main DSL and the first file argument" do
+    DatedBackup::ExecutionContext.should_receive(:new).with(:main, @filename).and_return nil
+    @evaluation_object.instance_eval File.read("bin/dbackup")
+  end
+
 end
