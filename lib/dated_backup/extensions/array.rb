74505e7596d095b0a1961e620d94f08a088b9acb
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-08-06 14:53:32 +0000

    moving reverse-sorted-unique-array into it's own file
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@175 8c6f25ac-f8d4-496d-a293-71e089142da2

--- lib/dated_backup/extensions/array.rb
+++ lib/dated_backup/extensions/array.rb
@@ -12,16 +12,3 @@ class Array
     DatedBackup::Core::BackupSet.new(self)
   end
 end
-
-# A subclass of Array, but it calls uniq!, sort!, and reverse!
-# (in that order) after the instance is created
-class ReverseSortedUniqueArray < Array
-  
-  def initialize *args, &blk
-    super *args, &blk
-    uniq!
-    sort!
-    reverse!
-  end    
-  
-end
