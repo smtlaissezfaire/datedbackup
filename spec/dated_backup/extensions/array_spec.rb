b1c89f1305ea7c832b73d5aab6113e039f946131
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-08-06 15:00:32 +0000

    moving specs accordingly (to last commit)
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@176 8c6f25ac-f8d4-496d-a293-71e089142da2

--- spec/dated_backup/extensions/array_spec.rb
+++ spec/dated_backup/extensions/array_spec.rb
@@ -1,22 +1,5 @@
 require File.dirname(__FILE__) + "/../../spec_helper"
 
-describe ReverseSortedUniqueArray do
-  it "should hold a unique array after initialization" do
-    ReverseSortedUniqueArray.new([1,2,3,1]).should == [3,2,1]
-  end
-  
-  it "should hold a reverse-sorted array after initialization" do
-    ReverseSortedUniqueArray.new([2,1,4,7,1,1]).should == [7,4,2,1]
-  end
-  
-  it "should be of class ReverseSortedUniqueArray" do
-    ReverseSortedUniqueArray.new().class.should == ReverseSortedUniqueArray
-  end
-  
-  it "should hold a unique array even after a method has changed the state of the array"
-  it "should hold a reverse-sorted unique array even after a method has changed the state of the array" 
-end
-
 describe Array, "car" do
   it "should return the first element of the array" do
     [1,2,3].car.should == 1
---
+++ spec/dated_backup/extensions/unique_array_spec.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+
+describe ReverseSortedUniqueArray do
+  it "should hold a unique array after initialization" do
+    ReverseSortedUniqueArray.new([1,2,3,1]).should == [3,2,1]
+  end
+  
+  it "should hold a reverse-sorted array after initialization" do
+    ReverseSortedUniqueArray.new([2,1,4,7,1,1]).should == [7,4,2,1]
+  end
+  
+  it "should be of class ReverseSortedUniqueArray" do
+    ReverseSortedUniqueArray.new().class.should == ReverseSortedUniqueArray
+  end
+  
+  it "should hold a unique array even after a method has changed the state of the array"
+  it "should hold a reverse-sorted unique array even after a method has changed the state of the array" 
+end
