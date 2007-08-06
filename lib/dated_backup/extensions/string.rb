d7426447e23d4d9b19a333d01536d9e9a6a5c822
smtlaissezfaire <smtlaissezfaire@8c6f25ac-f8d4-496d-a293-71e089142da2>
2007-08-06 14:04:47 +0000

    some error checking on the String#to_time method
    git-svn-id: http://datedbackup.rubyforge.org/svn/trunk@170 8c6f25ac-f8d4-496d-a293-71e089142da2

--- lib/dated_backup/extensions/string.rb
+++ lib/dated_backup/extensions/string.rb
@@ -1,4 +1,8 @@
 
+class StringToTimeConversionError < RuntimeError
+  
+end
+
 class String
   # This must take a time in a format similar to the
   # format generated the Time.to_string.  The format should be like this:
@@ -8,8 +12,14 @@ class String
   # conform to the time format.
   def to_time
     if self =~ /(\d{4}\-\d{2}-\d{2}-\d{2}h\-\d{2}m-\d{2}s)/  
-      time_array = $1.gsub(/h|m|s/, '').split '-'
-      Time.gm(*time_array)
+      begin
+        time_array = $1.gsub(/h|m|s/, '').split '-'
+        Time.gm(*time_array)
+      rescue 
+        raise StringToTimeConversionError, "The string cannot be a converted to a valid time (it is out of range)"
+      end
+    else
+      raise StringToTimeConversionError, "The string cannot be converted to a time object"
     end
   end
 end
--- spec/dated_backup/extensions/string_spec.rb
+++ spec/dated_backup/extensions/string_spec.rb
@@ -9,5 +9,15 @@ describe String, "to_time" do
     "/var/backups/network/backups/shares/2007-08-03-01h-05m-15s".to_time.should == Time.gm('2007', '08', '03', '01', '05', '15')
   end
   
-  it "should raise a TimeError if the String cannot be converted to a Time Object" 
+  it "should raise a StringToTimeConversionError if the String cannot be converted to a Time Object" do
+    lambda { 
+      "gobbly-gookhms0000-00asdfad-m00s".to_time
+    }.should raise_error(StringToTimeConversionError, "The string cannot be converted to a time object")
+  end
+  
+  it "should raise a StringToTimeConversionError if the String contains a time which would raise a time out of range error" do
+    lambda {
+      "/var/1750-00-00-00h-00m-00s".to_time
+    }.should raise_error(StringToTimeConversionError, "The string cannot be a converted to a valid time (it is out of range)")
+  end
 end
