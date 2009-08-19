164f400acfa701def94d31c1447fc4e6a0791152
Scott Taylor <scott@railsnewbie.com>
2009-08-19 19:46:06 -0400

    Remove require 'rubygems' (move it to the specs)

--- lib/dated_backup.rb
+++ lib/dated_backup.rb
@@ -1,7 +1,6 @@
 # DatedBackup, A snapshot backup utility
 # Copyright (C) Scott Taylor (scott@railsnewbie.com), 2007-2009
 
-require 'rubygems'
 require File.dirname(__FILE__) + "/dated_backup/extensions"
 require File.dirname(__FILE__) + "/dated_backup/dsl"
 require File.dirname(__FILE__) + "/dated_backup/core"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 
+require 'rubygems'
 require File.dirname(__FILE__) + "/../lib/dated_backup"
 
 $time_symbols = TimeSymbol.all
